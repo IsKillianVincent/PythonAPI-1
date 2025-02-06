eca92a16f7a744059215a1e0e5f9ee7bac9d2dfd
IsKillianVincent <pro.killianvincent@gmail.com>
2025-02-06 21:52:27 +0100

    added currency conversion route

---
+++ app/routes/conversion_route.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException, Request
+from app.services.exchange_service import fetch_exchange_rate
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+router = APIRouter()
+limiter = Limiter(key_func=get_remote_address)
+
+@limiter.limit("5/minute")
+@router.get("/convert")
+async def convert(request: Request, amount: float, from_currency: str = "eur", to_currency: str = "usd", date: str = "latest"):
+    rate = await fetch_exchange_rate(from_currency.lower(), to_currency.lower(), date)
+    converted_amount = round(amount * rate, 2)
+
+    return {
+        "amount": amount,
+        "from": from_currency.upper(),
+        "to": to_currency.upper(),
+        "date": date,
+        "exchange_rate": rate,
+        "converted_amount": converted_amount
+    }
