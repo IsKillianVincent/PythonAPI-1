daed55522fa111ea7b34a0f07b676cce73be76cd
TristanLBD <lbdtristan@gmail.com>
2025-02-06 14:16:38 +0100

    Fetching and adding the currencies from the API to the DB

--- app/database.py
+++ app/database.py
@@ -2,17 +2,14 @@ from peewee import MySQLDatabase
 from dotenv import load_dotenv
 import os
 
-# Charger le fichier .env
 load_dotenv()
 
-# Récupérer les info de connexion à la bd
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "pythonapi1")
 
-# Connexion à MySQL avec Peewee
 db = MySQLDatabase(DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=int(DB_PORT))
 
 def connect_db():
--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
+import requests
+from peewee import IntegrityError
 from database import connect_db, close_db
 from models.currencies import Currency
 from models.rate_conversion import RateConversion
 
-# Test de la connexion à la bd et creation de table
+CURRENCY_LIST_API_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json"
+def fetchAndInsertCurrencies():
+    """Récupère les données de l'API et insère les devises dans la base de données."""
+    try:
+        response = requests.get(CURRENCY_LIST_API_URL)
+        response.raise_for_status()
+
+        data = response.json()
+
+        for code, name in data.items():
+            try:
+                Currency.create(code=code, name=name)
+                print(f"✅ Insertion effectuée : {code} - {name}")
+            except IntegrityError:
+                print(f"⚠️ La devise {code} existe déja.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"❌ Une erreur est survenue lors de la récupération des devises : {e}")
+
+
 if __name__ == "__main__":
     connect_db()
     print("Database ouvert")
@@ -11,5 +32,7 @@ if __name__ == "__main__":
     RateConversion.create_table()
     print("Tables crées")
 
+    fetchAndInsertCurrencies()
+    
     close_db()
     print("Database fermer")
