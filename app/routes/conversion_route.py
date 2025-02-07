1dd59d09780010ebd87de1ec2e9307ebefa5ebac
TristanLBD <lbdtristan@gmail.com>
2025-02-07 11:12:13 +0100

    Adding the possibility to convert multiple amounts at once, from one currency to another

--- app/routes/conversion_route.py
+++ app/routes/conversion_route.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException, Request, Depends
+from fastapi import APIRouter, HTTPException, Request, Depends, Query
 from app.services.exchange_service import fetch_exchange_rate
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from typing import List
 
 router = APIRouter()
 limiter = Limiter(key_func=get_remote_address)
@@ -19,4 +20,30 @@ async def convert(request: Request, amount: float, from_currency: str = "eur", t
         "date": date,
         "exchange_rate": rate,
         "converted_amount": converted_amount
+    }
+
+@limiter.limit("5/minute")
+@router.get("/converts")
+async def converts(
+    request: Request,
+    amounts: List[float] = Query(..., description="Liste des montants à convertir"),
+    from_currency: str = "eur",
+    to_currency: str = "usd",
+    date: str = "latest"
+):
+    rate = await fetch_exchange_rate(from_currency.lower(), to_currency.lower(), date)
+    conversions = [
+        {
+            "amount": amount,
+            "converted_amount": round(amount * rate, 2)
+        }
+        for amount in amounts
+    ]
+
+    return {
+        "from": from_currency.upper(),
+        "to": to_currency.upper(),
+        "date": date,
+        "exchange_rate": rate,
+        "conversions": conversions
     }
