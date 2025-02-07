4f2a6113c32c6e31b5305209b6fad976764f151a
IsKillianVincent <pro.killianvincent@gmail.com>
2025-02-07 16:29:51 +0100

    Fix middleware api

--- app/middlewares/api_key_middleware.py
+++ app/middlewares/api_key_middleware.py
@@ -6,9 +6,14 @@ async def api_key_middleware(request: Request, call_next):
         return await call_next(request)
     
     api_key = request.headers.get("Authorization")
+    
+    if api_key and api_key.startswith("Bearer "):
+        api_key = api_key[len("Bearer "):]
+    
     print(f"Expected: {API_KEY}, Received: {repr(api_key)}")
     
     if api_key != API_KEY:
         print(f"Bearer err: {repr(API_KEY)}, Received: {repr(api_key)}")
         raise HTTPException(status_code=403, detail="Unauthorized access")
+    
     return await call_next(request)
